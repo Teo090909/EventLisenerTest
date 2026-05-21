e4d837e5dc28857d25d8170799b5dc233241383e
Teo090909 <teo.norlinvirkkala@elev.ga.ntig.se>
2026-05-21 12:49:25 +0200

    ändringar (eventlienertest

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log(DetFungerar);
+
+
+document.addEventListener("keydown", (event) => {
+
+console.log(event.key);    
+
+if(event.key == "t"){
+document.getElementById("a").style= "colur:red"
+}
+
+});
